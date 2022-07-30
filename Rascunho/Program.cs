5c4c0123f1f044658a97273965b39760ffb4d977
Ediee-sec <emersonrox8@gmail.com>
2022-07-30 15:12:24 -0300

    first commit

---
+++ Rascunho/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Tab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+
+            // Interação no numero maximo que um usuário pode consultar as tabuadas
+            for (j = 1; j <= 3; j++)
+            {
+                int Numero;
+                int Cont = 0;
+
+                // Tratamento de erros
+                try
+                {
+                    // Entrada de dados do usuário
+                    Console.Write("Qual Tabuada Deseja Consultar: ");
+                    Numero = int.Parse(Console.ReadLine());
+
+                    // Loop enquanto a variavel Cont for menor ou igual a 10
+                    while (Cont <= 10)
+                    {
+                        // Exibe o retorno da tabuada na tela
+                        Console.WriteLine(Math.Abs(Numero) + " X " + Cont + " = " + Tabuada(Numero, Cont));
+                        Cont++; // Incremento a variavel
+                    }
+                }
+                // Se houver erro
+                catch (Exception)
+                { Console.WriteLine("É permitido apenas números"); }
+
+                if(j == 3)
+                { Console.WriteLine("\n\nVocê já realizou o maximo de operações"); }       
+            }
+          
+        }
+
+        //Funcao que realiza o calculo da tabuada
+        static int Tabuada(int a,int b)
+        {
+            int resultado = Math.Abs(a * b);
+            return resultado;
+        }
+    }
+}
